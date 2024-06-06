dfdc0b5c31912691f92c12f5061880b36df93008
blueflylabor <Carrawayang@gmail.com>
2024-06-06 11:35:27 +0800

    run flash script

--- treeGen.py
+++ treeGen.py
@@ -3,7 +3,7 @@ dir = './posts/'
 mds = os.listdir(dir)
 mds.sort()
 lis = []
-url = 'https://github.com/nthr3ads/nthr3ads/blob/master/md/'
+url = 'https://github.com/blueflylabor/blueflylabor/blob/master/md/'
 for i in mds:
     st = '- [' + i.split('.md')[0] + '](' + url + i + ')'
     lis.append(st)
@@ -11,12 +11,12 @@ lis.sort()
 
 rd0 = """# Welcome to my homepage
 
-![nthr3ads's GitHub stats](https://github-readme-stats.vercel.app/api?username=nthr3ads&count_private=true&theme=dark)
+![blueflylabor's GitHub stats](https://github-readme-stats.vercel.app/api?username=blueflylabor&count_private=true&theme=dark)
 
-![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=nthr3ads&layout=compact&count_private=true&theme=dark)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs?username=blueflylabor&layout=compact&count_private=true&theme=dark)
 
 ## Website [temp use waiting for finishing post sys]
-- [https://nthr3ads.github.io](https://nthr3ads.github.io)
+- [https://blueflylabor.github.io](https://blueflylabor.github.io)
 
 
 ## Markdown Tree 
