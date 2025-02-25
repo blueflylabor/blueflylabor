288e42ea808cc6bc1be4c0dc141ed0ee430b5c87
藍翔技工 <48398805+blueflylabor@users.noreply.github.com>
2025-02-26 00:22:32 +0800

    run flash script

--- RUNME.bat
+++
@@ -1,4 +0,0 @@
-python tableGen.py
-call ./flash.bat
-cd ../blueflylabor.github.io
-call ./flash.bat
--- RUNME.sh
+++
@@ -1,5 +0,0 @@
-cp ./blueflylabor.github.io/_posts/*.md  ./posts/
-python3 tableGen.py
-bash ./flash.sh
-cd ./blueflylabor.github.io/
-bash ./flash.sh
